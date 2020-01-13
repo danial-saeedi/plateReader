3f97efac8998c9aba3946c8f8812664e7a63dd43
Danial Saeedi <saeedi.danial@gmail.com>
2020-01-13 23:39:16 +0330

    Reading the plate number

--- main.cpp
+++ main.cpp
@@ -5,6 +5,12 @@ using namespace std;
 #define M 8
 #define N 40
 
+struct Point
+{
+	int x;
+	int y;
+};
+
 void readPlate(int plate[M][N])
 {
 	ifstream f;
@@ -51,7 +57,7 @@ void readDigits(int digits[10][5][3])
 
 int noiseCounter(int digit,int row,int col,int plate[][40],int digits[][5][3])
 {
-	int errors = 0;
+	int noise = 0;
 
 	for(int i = 0; i < 5;i++)
 	{
@@ -59,12 +65,61 @@ int noiseCounter(int digit,int row,int col,int plate[][40],int digits[][5][3])
 		{
 			if(plate[row+i][col+j] != digits[digit][i][j])
 			{
-				errors++;
+				noise++;
 			}
 		}
 	}
 
-	return errors;
+	return noise;
+}
+
+int ExtractLicensePlate(int plate[][40],int digits[10][5][3])
+{
+	//We don't know where numbers starts in plate matrix. we have to estimate where is starts
+	Point start = {0,0};
+	bool endOfLoop = false;
+	for(int i = 0;i < M;i++)
+	{
+		if(endOfLoop == true) break;
+		for(int j = 0; j < N;j++)
+		{
+			if(endOfLoop == true) break;
+
+			for(int k = 0; k < 10; k++)
+			{
+				int e = noiseCounter(k,i,j,plate,digits);
+				if(e == 0)
+				{
+					start.x = i;
+					start.y = j;
+					endOfLoop = true;
+					break;
+				}
+			}
+		}
+	}
+
+	string number;
+	for(int q = 1;q < 5; q++)
+	{
+		start.y += 5*q;
+		int lowestError = 9999;
+		int digit = 0;
+		for(int k = 0;k < 10;k++)
+		{
+			int e = noiseCounter(k,start.x,start.y,plate,digits);
+
+			if(e < lowestError)
+			{
+				digit = k;
+				lowestError = e;
+			}
+		}
+
+		number += to_string(digit);
+	}
+
+	return stoi(number);
 }
 
 int main()
@@ -75,32 +130,32 @@ int main()
 
 	readDigits(digits);
 
-	cout << noiseCounter(0,1,3,plate,digits) << endl;
+	cout << "Plate No : " <<  ExtractLicensePlate(plate,digits) << endl << endl;
 
-	// cout << "Plate.txt : " << endl;
-	// for(int k = 0; k < M;k++)
-	// {
-	// 	for(int q = 0;q < N; q++)
-	// 	{
-	// 		cout << plate[k][q];
-	// 	}
+	cout << "Plate.txt : " << endl;
+	for(int k = 0; k < M;k++)
+	{
+		for(int q = 0;q < N; q++)
+		{
+			cout << plate[k][q];
+		}
 
-	// 	cout << endl;
-	// }
+		cout << endl;
+	}
 
-	// cout << endl << "Digits.txt : " << endl;
-	// for(int k = 0;k < 10;k++)
-	// {
-	// 	for(int q = 0;q<5;q++)
-	// 	{
-	// 		for(int p = 0; p < 3;p++)
-	// 		{
-	// 			cout << digits[k][q][p];
-	// 		}
-	// 		cout << endl;
-	// 	}
-	// 	cout << endl;
-	// }
+	cout << endl << "Digits.txt : " << endl;
+	for(int k = 0;k < 10;k++)
+	{
+		for(int q = 0;q<5;q++)
+		{
+			for(int p = 0; p < 3;p++)
+			{
+				cout << digits[k][q][p];
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
 
 	return 0;
 }
